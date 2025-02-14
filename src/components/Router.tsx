206bd2a6beefcf6b8ed1808301bd608d9db37f7c
santa-developer <finalatro37@naver.com>
2025-02-14 16:14:39 +0900

    feat: 컴포넌트와 라우트 연결

---
+++ src/components/Router.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import HomePage from "pages/home";
+import PostListPage from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostEdit from "pages/posts/edit";
+import PostNew from "pages/posts/new";
+import ProfilePage from "pages/profile";
+import ProfileEdit from "pages/profile/edit";
+import SearchPage from "pages/search";
+import NotificationsPage from "pages/notifications";
+import LoginPage from "pages/users/login";
+import SignupPage from "pages/users/signup";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/posts' element={<PostListPage />} />
+      <Route path='/posts/:id' element={<PostDetail />} />
+      <Route path='/posts/new' element={<PostNew />} />
+      <Route path='/posts/edit' element={<PostEdit />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/profile/edit' element={<ProfileEdit />} />
+      <Route path='/notifications' element={<NotificationsPage />} />
+      <Route path='/search' element={<SearchPage />} />
+      <Route path='/users/login' element={<LoginPage />} />
+      <Route path='/users/signup' element={<SignupPage />} />
+      <Route path='*' element={<Navigate replace to='/' />} />
+    </Routes>
+  );
+};
+
+export default Router;
