ccce65f0d8e1ee0125e6fa0253506be938856f72
santa-developer <finalatro37@naver.com>
2025-02-15 19:59:47 +0900

    design: 상단 타이틀, 탭 영역 고정되게 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -54,11 +54,14 @@ const posts: PostProps[] = [
 const HomePage = () => {
   return (
     <div className='home'>
-      <div className='home__title'>Home</div>
-      <div className='home__tabs'>
-        <div className='home__tab home__tab--active'>For You</div>
-        <div className='home__tab'>Following</div>
+      <div className='home__top'>
+        <div className='home__title'>Home</div>
+        <div className='home__tabs'>
+          <div className='home__tab home__tab--active'>For You</div>
+          <div className='home__tab'>Following</div>
+        </div>
       </div>
+
       {/* post form */}
       <PostForm />
 
