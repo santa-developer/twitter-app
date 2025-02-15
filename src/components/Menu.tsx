a0432a7d11c6b0ee16264461bd03073eb4bf7bc3
santa-developer <finalatro37@naver.com>
2025-02-15 16:12:30 +0900

    feat: 로그아웃 기능 추가

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,11 +5,27 @@ import { TbLogout, TbLogin } from "react-icons/tb";
 
 import { useContext } from "react";
 import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 const MenuList = () => {
   const navigate = useNavigate();
 
   const { user } = useContext(AuthContext);
+
+  // 로그아웃 이벤트
+  const handleLogout = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+
+      toast.success("로그아웃 되었습니다.");
+      navigate("/users/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
   return (
     <div className='footer'>
       <div className='footer__grid'>
@@ -24,7 +40,7 @@ const MenuList = () => {
             <TbLogin /> LogIn
           </button>
         ) : (
-          <button type='button' onClick={() => navigate("/")}>
+          <button type='button' onClick={handleLogout}>
             <TbLogout /> Logout
           </button>
         )}
