09ee62a309c836ddc8627801d740190d701468ab
santa-developer <finalatro37@naver.com>
2025-02-15 12:32:54 +0900

    feat: observer 추가하여 인증상태 확인 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import Layout from "components/Layout";
 import Router from "components/Router";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   const auth = getAuth(app);
   const [isAuth, setIsAuth] = useState<boolean>(!!auth?.currentUser);
+  const [init, setInit] = useState<boolean>(false);
 
   // console.log(auth, isAuth);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+
+      setInit(true);
+    });
+  }, [auth]); //auth 값이 바뀔 때 마다 호출
   return (
     <Layout>
       <ToastContainer />
-      <Router isAuth={isAuth} />
+      {/* 상태값이 변경이 됐는지 안됐는지 확인 후에 Router를 보여줌 */}
+      {init ? <Router isAuth={isAuth} /> : <>loading...</>}
     </Layout>
   );
 }
